bb57297ef86b81ed6facd13ff73d25d1b2966a73
markaxelus <mrkaxelus@gmail.com>
2024-10-31 16:10:04 -0700

    feat(header,nav): add header and nav files init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+// Persistent components
+import Header from '../components/Header'
+
 const ClashGrotesk = localFont({
   src: "./fonts/ClashGrotesk-Regular.otf",
   variable: "--font-clashGrotesk",
@@ -20,9 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${ClashGrotesk.variable} antialiased`}
-      >
+      <body className={`${ClashGrotesk.variable} antialiased`} >
+        
+        <Header />
         {children}
       </body>
     </html>
