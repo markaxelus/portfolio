76ed33cb90b27223aca087e8cd0547d5c57e1e0e
markaxelus <mrkaxelus@gmail.com>
2024-10-31 18:34:25 -0700

    style(navbar): add responsiveness and animations styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,25 +4,27 @@ import { Button } from './ui/button'
 // Components
 import Navbar from './Navbar'
 
+import styles from "./Navbar.module.css"
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-primary capitalize">
+    <header className="py-4 xl:py-0 text-primary capitalize border-b ">
         <div className="container mx-auto flex justify-between items-center">
             <Link href='/'>
-                <h1 className='text-4xl font-semibold'>
-                    mark<span className='text-accent'> .</span>
+            <div className="flex items-center">
+                <h1 className="text-4xl font-semibold">
+                    mark<span className="text-accent">.</span>
                 </h1>
+            </div>
             </Link>
 
-        {/* Desktop Nav */}
-            <div className="hidden xl:flex items-center gap-8">
+        {/* Desktop Nav & Contact Me*/}
+            <div className="hidden xl:flex items-center justify-center ">
                 <Navbar />
-                <Link href="/contact">
-                    <Button className="bg-red-400 capitalize">
-                        contact me
-                    </Button>
-                </Link>
             </div>
+
+            <Link href="/contact" className={`hidden xl:flex gap-1 items-center justify-center border rounded-full w-44 h-12  ${styles.navLink}`}>
+                <img src='/icons/email.png' className={`${styles.email} `} />contact me 
+            </Link>
             
         {/* Mobile Nav */}
             <div className='xl:hidden'>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,37 @@
+"use client";
+import Link from 'next/link'
+import { usePathname } from "next/navigation"
+
+import styles from './Navbar.module.css';
+
+const navLinks = [
+  {
+    name: "home",
+    path: "/"
+  },
+  {
+    name: "about",
+    path: "/about",
+  },
+  {
+    name: "projects",
+    path: "/project",
+  },
+]
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <nav>
-      desktop nav
+    <nav className="flex ">
+      {navLinks.map((link,index) => {
+          return(
+            <Link href={link.path} key={index} className={`flex justify-center items-center w-56 h-32 border-x ${styles.navLink}
+                                                            ${link.path === pathname && "bg-primary text-mainbg"}`}> 
+              {link.name}
+            </Link>  // Using key is for performance and correct rendering
+          );
+        })}
     </nav>
   )
 }
