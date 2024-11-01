f507260c7b663484f4149ba7afaccf024c410fda
markaxelus <mrkaxelus@gmail.com>
2024-11-01 01:11:05 -0700

    style(navbar): add main styling, transition, animations, and icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,35 +5,33 @@ import { Button } from './ui/button'
 import Navbar from './Navbar'
 
 import styles from "./Navbar.module.css"
-const Header = () => {
-  return (
+const Header = () => (
     <header className="py-4 xl:py-0 text-primary capitalize border-b ">
         <div className="container mx-auto flex justify-between items-center">
             <Link href='/'>
-            <div className="flex items-center">
-                <h1 className="text-4xl font-semibold">
-                    mark<span className="text-accent">.</span>
-                </h1>
-            </div>
+                <div className="flex items-center">
+                    <h1 className="text-5xl font-semibold">
+                        mark<span className="text-accent">.</span>
+                    </h1>
+                </div>
             </Link>
 
-        {/* Desktop Nav & Contact Me*/}
+            {/* Desktop Nav & Contact Me*/}
             <div className="hidden xl:flex items-center justify-center ">
                 <Navbar />
             </div>
 
-            <Link href="/contact" className={`hidden xl:flex gap-1 items-center justify-center border rounded-full w-44 h-12  ${styles.navLink}`}>
-                <img src='/icons/email.png' className={`${styles.email} `} />contact me 
+            <Link href="/contact" className={`hidden xl:flex text-xl gap-2 items-center justify-center border rounded-full w-44 h-12  ${styles.navLink}`}>
+                <img src='/icons/email.png' className={`${styles.email} `} />contact me
             </Link>
-            
-        {/* Mobile Nav */}
+
+            {/* Mobile Nav */}
             <div className='xl:hidden'>
                 mobile
             </div>
 
         </div>
     </header>
-  )
-}
+)
 
 export default Header
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,55 @@
 "use client";
-import Link from 'next/link'
-import { usePathname } from "next/navigation"
+import Link from 'next/link';
+import { usePathname } from "next/navigation";
+import Image from 'next/image';
 
 import styles from './Navbar.module.css';
 
 const navLinks = [
   {
     name: "home",
-    path: "/"
+    path: "/",
+    icon: "/icons/home.png",
   },
   {
     name: "about",
     path: "/about",
+    icon: "/icons/about.png",
   },
   {
     name: "projects",
-    path: "/project",
+    path: "/projects",
+    icon: "/icons/projects.png",
   },
-]
+  
+];
 
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="flex ">
-      {navLinks.map((link,index) => {
-          return(
-            <Link href={link.path} key={index} className={`flex justify-center items-center w-56 h-32 border-x ${styles.navLink}
-                                                            ${link.path === pathname && "bg-primary text-mainbg"}`}> 
-              {link.name}
-            </Link>  // Using key is for performance and correct rendering
-          );
-        })}
+    <nav className="flex">
+      {navLinks.map((link, index) => {
+        const isActive = link.path === pathname;
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`flex justify-center items-center w-56 h-32 gap-2 text-xl border-x ${styles.navLink} ${isActive ? styles.activeLink : ""} ${isActive ? "bg-primary text-mainbg" : ""}`}
+          >
+            <Image
+              src={link.icon}
+              alt={`${link.name} icon`}
+              width={17} 
+              height={17}
+              className={styles.icons}
+            />
+            {link.name}
+          </Link>
+        );
+      })}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
