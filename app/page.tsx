2fb7f51d3b2dfa268eb021fcbdb1edfc693f7fde
markaxelus <mrkaxelus@gmail.com>
2025-03-21 03:51:01 -0700

    feat: hero section init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image"; 
-import Navbar from "@/components/navbar";
-
+import Hero from "@/components/hero"
 
 export default function Home() {
   return (
-    <Navbar />
+    <div>
+      <Hero />
+    </div>
   );
 }
---
+++ components/hero.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from "@/components/navbar"
+
+const hero = () => {
+  return (
+    <div>
+        Mark Axelus
+
+        {/* Intro */}
+        <div>
+          <span>Full Stack Developer</span>
+          <p>Research → UI/UX → Prototyping → Product → Marketing </p>
+        </div>
+        <div>
+          <Navbar />
+        </div>
+
+    </div>
+  )
+}
+
+export default hero
