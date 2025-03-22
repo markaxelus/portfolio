823267cc4816fd1c25e0a1ed38a07a2054ac9fd0
markaxelus <mrkaxelus@gmail.com>
2025-03-21 21:32:00 -0700

    styling fixes for hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image"; 
 import Hero from "@/components/hero"
+import Projects from "@/components/Projects"
 
 export default function Home() {
   return (
     <div className="font-inter">
       <Hero />
+      <Projects />
     </div>
   );
 }
