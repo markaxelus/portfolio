b7a855b99bce173ece177a788f3bb266493bcf49
markaxelus <mrkaxelus@gmail.com>
2025-06-15 03:47:22 -0700

    init

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Sun, Moon } from "lucide-react"
+
+const index = () => {
+  return (
+    <div className='h-[81px] bg-red-200'>
+      <div className='px-[30px] py-[24px]'>
+        {/* Name Heading */}
+        <div className="text-3xl font-bold">
+          MARK
+        </div>
+
+        {/* Right-Side Navigation */}
+        <div className="div">
+          <div className="div">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default index
