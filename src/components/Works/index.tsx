82315b9d5df0c7fa550fdab384d9f3273420af79
markaxelus <mrkaxelus@gmail.com>
2025-06-17 08:47:29 -0700

    img for works

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -19,8 +19,9 @@ const index = ({ headingText, headingClassName }: WorksProps) => {
       {/* Heading */}
       <div className="flex justify-between">
         <h1 className={`${headingClassName}`}>{headingText}</h1>
-        <Link href='/works'
-          className="bg-[rgb(245,245,245)] w-[133px] h-[51px] "  
+        <Link
+          href="/works"
+          className="bg-[rgb(245,245,245)] w-[133px] h-[51px] "
         >
           <button className=" w-full h-full tracking-[-0.04em] font-medium">
             Show more
@@ -31,33 +32,42 @@ const index = ({ headingText, headingClassName }: WorksProps) => {
       {/* Display Section */}
       <div className="pt-12">
         {projects.map((project: Project) => (
-          <Link 
+          <Link
             key={project.id}
             href={`/works/${project.slug}`}
             className="group relative block border-[rgb(229, 231, 245)] border-b py-8 overflow-hidden"
           >
             <div className="flex justify-between px-[96px] py-[60px]  min-h-[160px] tracking-[-0.04em]">
-
               {/* Left Container: id, title, date */}
               <div className=" flex gap-3 ">
                 <span className="pt-3.5 text-gray-400 ">0{project.id}</span>
                 <div className="">
-                  <h3 className="text-[48px] tracking-[-0.04em]">{project.title}</h3>
+                  <h3 className="text-[48px] tracking-[-0.04em]">
+                    {project.title}
+                  </h3>
                   <span className="text-gray-600 ">{project.date}</span>
                 </div>
-                
               </div>
 
-              {/* Right Container: category */}
+              {/* Right Container: category, stack */}
               <div className="flex flex-col justify-start items-end text-right">
-  <span className="text-[28px] tracking-[-0.04em]">{project.category}</span>
-  <span className="text-[16px] tracking-[-0.04em] text-gray-400 break-words max-w-[400px] leading-5">
-    {project.stack.join(', ')}
-  </span>
-</div>
+                <span className="text-[28px] tracking-[-0.04em]">
+                  {project.category}
+                </span>
+                <span className="text-[16px] tracking-[-0.04em] text-gray-400 break-words max-w-[400px] leading-5">
+                  {project.stack.join(", ")}
+                </span>
+              </div>
 
+              {/* Hover Img */}
+              <img 
+                src={project.imageUrl}
+                alt={project.title}
+                className="
+                 pointer-events-none w-60 fixed rounded-lg shadow-lg transition-opacity duration-150 ease-out
+                "
+              />
             </div>
-
           </Link>
         ))}
       </div>
