14c7ec669746871a3dc0ae46add6bc027ed9dcbb
markaxelus <mrkaxelus@gmail.com>
2025-06-17 00:15:24 -0700

    initial projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Hero from "@/components/Hero"
+import SelectedProjects from "@/components/Projects/SelectedProjects"
 
 export default function Home() {
   return (
     <main className="">
       <Hero/>
+      <SelectedProjects
+        headingText="work."
+        headingClassName="text-[30px] tracking-[-0.03em] font-medium"
+      />
     </main>
   );
 }
--- src/components/Projects/SelectedProjects/index.tsx
+++ src/components/Projects/SelectedProjects/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Link from 'next/link'
-import { projects } from "@/data/projects"
+import React from "react";
+import Link from "next/link";
+import { Project, projects } from "@/data/projects";
 
-const index: React.FC = () => {
-  return (
-    <section className=''>
-      <h1 className="text"></h1>
-    </section>
-  )
+interface SelectedProjectsProps {
+  headingText: string;
+  headingClassName: string;
 }
 
-export default index
+const index = ({ headingText, headingClassName }: SelectedProjectsProps) => {
+  return (
+    <section
+      className="max-w-[1600px] w-full mx-auto
+          h-[100vh] 
+          px-[20px] md:px-[30px] lg:px-0
+          flex flex-col md:justify-between 
+          gap-12 md:gap-6"
+    >
+      <div className="flex justify-between">
+        <h1 className={`${headingClassName}`}>{headingText}</h1>
+        <Link href='/works'
+          className="bg-[rgb(245,245,245)] w-[133px] h-[51px] "  
+        >
+          <button className=" w-full h-full tracking-[-0.04em] font-medium">
+            Show more
+          </button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default index;
