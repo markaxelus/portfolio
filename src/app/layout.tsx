4c2443016cb48a96ee9c24c9eceea1e8c3423fb4
Mark <mrkaxelus@gmail.com>
2025-06-24 12:35:49 -0700

    metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Mark | Portfolio",
+  title: "Mark Axelus| Portfolio",
   description: "Welcome to my portfolio!",
 };
 
