eab685ca9bffdc0bee88c417b9317a879e76c6ff
markaxelus <mrkaxelus@gmail.com>
2025-07-16 01:10:51 -0700

    pointer type check for different devices to render

--- src/utils/EmailCopy/index.tsx
+++ src/utils/EmailCopy/index.tsx
@@ -28,7 +28,7 @@ const EmailCopy = ({ email, className }: EmailCopyProps) => {
         onClick={handleCopy}
         data-cursor="copy"
         data-copied={copied}
-        className="flex cursor-pointer text-inherit p-1 items-center gap-2"
+        className="flex cursor-pointer text-inherit items-center gap-2"
         aria-label="Copy email"
         title="Copy email"
       >
