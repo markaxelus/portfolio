55a9650cbac1a9d27be8d1c008dd943a14fbcdd7
markaxelus <mrkaxelus@gmail.com>
2025-09-16 02:21:50 -0700

    cypress smoke test

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,15 +1,12 @@
 describe("navigation smoke", () => {
   it("loads home and navigates to Works, About, Contact", () => {
-    // Avoid hitting external Redis by setting cookie + stubbing
     cy.setCookie("ignoreVisits", "true");
     cy.intercept("GET", "/api/visits", { statusCode: 200, body: { total: 0 } });
 
     cy.visit("/");
 
-    // Navbar brand (ensure link role, avoid footer text)
     cy.findByRole("link", { name: /^mark$/i }).should("exist");
 
-    // Open nav menu (limit to navbar scope to avoid other buttons)
     cy.get("nav").within(() => {
       cy.get("button").first().click();
     });
